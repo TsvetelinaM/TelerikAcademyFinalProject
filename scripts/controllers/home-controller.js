a8cac3cf610e23dcd5160239fa9da63eda5be1b7
TsvetelinaM <tsvetka_ta@abv.bg>
2017-10-07 11:01:20 +0300

    finished single-article view

--- scripts/controllers/home-controller.js
+++ scripts/controllers/home-controller.js
@@ -9,46 +9,64 @@ const array = [
     {imageURL:'./../styles/imgs/flower1.jpeg', title:'Image test 3'}
 ];
 
-function all(context) {
-    templates.get('home').then(function(template) {
-        context.$element().html(template(array));
 
-        (function attach() {
-            $('#carousel_container').on('click', function(ev) {
-                const elements = $('.carrousel__image'),
-                    container = $('.carrousel__image__container'),
-                    active = $('.carrousel__image--center'),
-                    imgSource = array,
-                    imgStorage = array,
+const imagesArray = array.splice(0, 5);
+const incomingImages = array;
+
+
+
+function all(context) {
+    templates.get('home').then((template) => {
+        context.$element().html(template(imagesArray));
+        
+        (() => {
+            $('.tm-carrousel-container').on('click', (ev) => {
+                let elements = $('.tm-carrousel-image'),
+                    container = $('.tm-carrousel-image-container'),
+                    active = $('.tm-carrousel-image-center'),
+                    imgSource = imagesArray,
+                    imgStorage = incomingImages,
                     len = imgSource.length,
                     target = $(ev.target);
         
                 if (target.hasClass('glyphicon-chevron-right')) {
-                    imgSource.push(imgStorage.shift());
-                    imgStorage.push(imgSource.shift());
-                    var elementToGo = elements.first(),
-                        elementToCome = $("<div><img src=" + imgSource[len - 1].imageUrl + "></div>").addClass('carrousel__image');
-                    elementToGo.addClass('shrink--right');
-                    container.append(elementToCome.addClass('grow--right'));
-                    setTimeout(function() {
-                        active.attr('src', imgSource[2].imageUrl);
-                        $('.carrousel__title').html(imgSource[2].title);
+                    if (imgStorage.length) {
+                        imgSource.push(imgStorage.shift());
+                        imgStorage.push(imgSource.shift());
+                    } else {
+                        imgSource.push(imgSource.slice(0,2)[0]);
+                        imgStorage.push(imgSource.shift());
+                    }
+                    
+                    let elementToGo = elements.first(),
+                        elementToCome = $("<div><img src=" + imgSource[len - 1].imageURL + "></div>").addClass('tm-carrousel-image');
+                    elementToGo.addClass('tm-shrink-right');
+                    container.append(elementToCome.addClass('tm-grow-right'));
+                    setTimeout(() => {
+                        active.attr('src', imgSource[2].imageURL);
+                        $('.tm-carrousel-title').html(imgSource[2].title);
                         elementToGo.remove();
-                        elementToCome.removeClass('grow--right');
+                        elementToCome.removeClass('tm-grow-right');
                     }, 350);
                 }
                 if (target.hasClass('glyphicon-chevron-left')) {
-                    imgSource.unshift(imgStorage.pop());
-                    imgStorage.unshift(imgSource.pop());
-                    var _elementToGo = $('.carrousel__image:nth-child(5)'),
-                        _elementToCome = $("<div><img src=" + imgSource[0].imageUrl + "></div>").addClass('carrousel__image');
-                    _elementToGo.addClass('shrink--left');
-                    container.prepend(_elementToCome.addClass('grow--left'));
-                    setTimeout(function() {
-                        active.attr('src', imgSource[2].imageUrl);
-                        $('.carrousel__title').html(imgSource[2].title);
-                        _elementToGo.remove();
-                        _elementToCome.removeClass('grow--left');
+                    if (imgStorage.length) {
+                        imgSource.unshift(imgStorage.pop());
+                        imgStorage.unshift(imgSource.pop());
+                    } else {
+                        imgSource.unshift(imgSource.slice(length-1)[0]);
+                        imgStorage.unshift(imgSource.pop());
+                    }
+                    
+                    let elementToGo = $('.tm-carrousel-image:nth-child(5)'),
+                        elementToCome = $("<div><img src=" + imgSource[0].imageURL + "></div>").addClass('tm-carrousel-image');
+                    elementToGo.addClass('tm-shrink-left');
+                    container.prepend(elementToCome.addClass('tm-grow-left'));
+                    setTimeout(() => {
+                        active.attr('src', imgSource[2].imageURL);
+                        $('.tm-carrousel-title').html(imgSource[2].title);
+                        elementToGo.remove();
+                        elementToCome.removeClass('tm-grow-left');
                     }, 350);
                 }
             });
